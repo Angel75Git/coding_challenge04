ae92b7d62ede396f9d8d32ce34898f1f41a53061
Angel75Git <aduboncenteno@usf.edu>
2025-02-02 18:54:55 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,4 +6,12 @@ if (purchaseAmount > 100){
     console.log(`Final amount after discount: $${finalAmount}`);
 };
 
+//Task 2
+let sales = [120, 85, 200, 150, 90]; //example of sales figures
+console.log("Sales: ", sales);
+sum = 0;  //sum of array needs to start at 0
+for (let i = 0; i < sales.length; i++){ // for loop goes through each index to sum
+    sum += sales[i];
+}
+console.log(`The total sum of sales: $${sum}`);
 
