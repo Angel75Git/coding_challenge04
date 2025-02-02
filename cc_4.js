fd53033468222dad84cc15d86b8d87be70d3ddd3
Angel75Git <aduboncenteno@usf.edu>
2025-02-02 11:53:07 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1
+//Checking to see if purchase amount qualifies for discount
+let purchaseAmount = 124.99;
+if (purchaseAmount > 100){
+    finalAmount =  purchaseAmount - 20.00;   
+    console.log(`Final amount after discount: $${finalAmount}`);
+};
+
+
